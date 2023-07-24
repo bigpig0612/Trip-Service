2cbe37ce5f4aecf0d499b921b3682fbd7a67a67c
yaolong <yaolong0612@gmail.com>
2023-07-24 22:16:44 +1000

    Added attributes for Tab and Trip

--- src/main/java/com/littlepay/tripservice/model/Tab.java
+++ src/main/java/com/littlepay/tripservice/model/Tab.java
@@ -1,6 +1,33 @@
 package com.littlepay.tripservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tab {
+
+    private Integer id;
+
+    private LocalDateTime dateTimeUTC;
+
+    private TabType tapType;
+
+    private String stopId;
+
+    private String companyId;
+
+    private String busID;
+
+    private String PAN;
+
+    public enum TabType {
+        ON,
+        OFF;
+    }
+
 }
--- src/main/java/com/littlepay/tripservice/model/Trip.java
+++ src/main/java/com/littlepay/tripservice/model/Trip.java
@@ -1,7 +1,42 @@
 package com.littlepay.tripservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Trip {
+
+    private LocalDateTime started;
+
+    private LocalDateTime finished;
+
+    private long durationSecs;
+
+    private String fromStopId;
+
+    private String toStopId;
+
+    private double chargeAmount;
+
+    private String companyId;
+
+    private String busID;
+
+    private String PAN;
+
+    private TripStatus status;
+
+    public enum TripStatus {
+        COMPLETED,
+        INCOMPLETE,
+        CANCELLED;
+    }
+
 }
