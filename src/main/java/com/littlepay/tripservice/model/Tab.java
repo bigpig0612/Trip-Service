2119d3adca57774986032c70501c82ed4f96501d
yaolong <yaolong0612@gmail.com>
2023-07-25 10:43:54 +1000

    Added CSV related annotation to Tab and Trip model

--- src/main/java/com/littlepay/tripservice/model/Tab.java
+++ src/main/java/com/littlepay/tripservice/model/Tab.java
@@ -1,5 +1,8 @@
 package com.littlepay.tripservice.model;
 
+import com.littlepay.tripservice.util.LocalDateTimeConverter;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvCustomBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,20 +14,51 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Tab {
 
+    /**
+     * The unique identifier for the tab.
+     */
+    @CsvBindByName(column = "ID")
     private Integer id;
 
+    /**
+     * The timestamp of the tab.
+     */
+    @CsvCustomBindByName(column = "DateTimeUTC", converter = LocalDateTimeConverter.class)
     private LocalDateTime dateTimeUTC;
 
+    /**
+     * The type of the tab.
+     */
+    @CsvBindByName(column = "TapType")
     private TabType tapType;
 
+    /**
+     * The ID of the stop where the tab occurred.
+     */
+    @CsvBindByName(column = "StopId")
     private String stopId;
 
+    /**
+     * The ID of the company operating the bus.
+     */
+    @CsvBindByName(column = "CompanyId")
     private String companyId;
 
+    /**
+     * The ID of the bus associated with the tab.
+     */
+    @CsvBindByName(column = "BusID")
     private String busID;
 
+    /**
+     * The PAN (Personal Account Number) of the customer associated with the tab.
+     */
+    @CsvBindByName(column = "PAN")
     private String PAN;
 
+    /**
+     * Enum representing the type of a tab, which can be ON or OFF.
+     */
     public enum TabType {
         ON,
         OFF;
--- src/main/java/com/littlepay/tripservice/model/Trip.java
+++ src/main/java/com/littlepay/tripservice/model/Trip.java
@@ -1,5 +1,9 @@
 package com.littlepay.tripservice.model;
 
+import com.littlepay.tripservice.util.LocalDateTimeConverter;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
+import com.opencsv.bean.CsvCustomBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,30 +17,84 @@ import java.time.LocalDateTime;
 @Builder
 public class Trip {
 
+    /**
+     * The timestamp when the trip started.
+     */
+    @CsvCustomBindByName(column = "Started", converter = LocalDateTimeConverter.class)
+    @CsvBindByPosition(position = 0)
     private LocalDateTime started;
 
+    /**
+     * The timestamp when the trip finished.
+     */
+    @CsvCustomBindByName(column = "Finished", converter = LocalDateTimeConverter.class)
+    @CsvBindByPosition(position = 1)
     private LocalDateTime finished;
 
+    /**
+     * The duration of the trip in seconds.
+     */
+    @CsvBindByName(column = "DurationSecs")
+    @CsvBindByPosition(position = 2)
     private long durationSecs;
 
+    /**
+     * The ID of the stop where the trip started.
+     */
+    @CsvBindByName(column = "FromStopId")
+    @CsvBindByPosition(position = 3)
     private String fromStopId;
 
+    /**
+     * The ID of the stop where the trip finished.
+     */
+    @CsvBindByName(column = "ToStopId")
+    @CsvBindByPosition(position = 4)
     private String toStopId;
 
+    /**
+     * The amount charged for the trip.
+     */
+    @CsvBindByName(column = "ChargeAmount")
+    @CsvBindByPosition(position = 5)
     private double chargeAmount;
 
+    /**
+     * The ID of the company operating the bus.
+     */
+    @CsvBindByName(column = "CompanyId")
+    @CsvBindByPosition(position = 6)
     private String companyId;
 
+    /**
+     * The ID of the bus used for the trip.
+     */
+    @CsvBindByName(column = "BusID")
+    @CsvBindByPosition(position = 7)
     private String busID;
 
+    /**
+     * The PAN (Personal Account Number) of the customer.
+     */
+    @CsvBindByName(column = "PAN")
+    @CsvBindByPosition(position = 8)
     private String PAN;
 
+    /**
+     * The status of the trip, which can be COMPLETED, INCOMPLETE, CANCELLED, or UNKNOWN.
+     */
+    @CsvBindByName(column = "Status")
+    @CsvBindByPosition(position = 9)
     private TripStatus status;
 
+    /**
+     * Enum representing the status of a trip.
+     */
     public enum TripStatus {
         COMPLETED,
         INCOMPLETE,
-        CANCELLED;
+        CANCELLED,
+        UNKNOWN;
     }
 
 }
